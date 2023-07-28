daf148e05d46946654309428e491b19fab4dc619
elifinesse <133097200+elifinesse@users.noreply.github.com>
2023-07-28 11:20:18 +0300

    hareket mesajları sorunlu + testlerin çoğu fail

--- frontend/components/AppFunctional.js
+++ frontend/components/AppFunctional.js
@@ -1,4 +1,5 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 // önerilen başlangıç stateleri
 const initialMessage = "";
@@ -24,6 +25,7 @@ export default function AppFunctional(props) {
   const [steps, setSteps] = useState(initialSteps);
   const [email, setEmail] = useState(initialEmail);
   const [index, setIndex] = useState(initialIndex);
+  const [data, setData] = useState({ x: 2, y: 2, steps: 0, email: "" });
 
   function getXY() {
     // Koordinatları izlemek için bir state e sahip olmak gerekli değildir.
@@ -57,49 +59,102 @@ export default function AppFunctional(props) {
 
   function onChange(evt) {
     setEmail(evt.target.value);
+    //setData({ ...data, email: email });
   }
-  function onClick() {
+
+  /*  function onClick() {
     setSteps(steps + 1);
+    setData({ ...data, steps: steps });
+  } */
+  function moveLeft() {
+    if (index % 3 === 0) {
+      setMessage("Sola gidemezsin");
+    } else {
+      setIndex(index - 1);
+      setSteps(steps + 1);
+      //setData({ ...data, x: xy[1] });
+    }
+  }
+  function moveUp() {
+    if (index < 4) {
+      setMessage("Yukarı gidemezsin");
+    } else {
+      setIndex(index - 3);
+      setSteps(steps + 1);
+      //setData({ ...data, y: xy[4] });
+    }
+  }
+  useEffect(() => {
+    setData({ ...data, x: xy[1], y: xy[4], steps: steps, email: email });
+    console.log(data);
+  }, [index, steps, email]);
+  function moveRight() {
+    if (index % 3 === 2) {
+      setMessage("Sağa gidemezsin");
+    } else {
+      setIndex(index + 1);
+      setSteps(steps + 1);
+      setData({ ...data, x: xy[1] });
+    }
+  }
+  function moveDown() {
+    if (index < 9 && index > 5) {
+      setMessage("Aşağı gidemezsin");
+    } else {
+      setIndex(index + 3);
+      setSteps(steps + 1);
+      setData({ ...data, y: xy[4] });
+    }
   }
 
   function onSubmit(evt) {
     // payloadu POST etmek için bir submit handlera da ihtiyacınız var.
+    evt.preventDefault();
+    console.log(evt);
+    axios
+      .post("http://localhost:9000/api/result", data)
+      .then((res) => {
+        setMessage(res.data.message);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
   }
 
   return (
     <div id="wrapper" className={props.className}>
       <div className="info">
-        <h3 id="coordinates">Koordinatlar (2, 2)</h3>
+        <h3 id="coordinates">{`Koordinatlar ${xy}`}</h3>
         <h3 id="steps">{`${steps} kere ilerlediniz`}</h3>
       </div>
       <div id="grid">
         {[0, 1, 2, 3, 4, 5, 6, 7, 8].map((idx) => (
-          <div key={idx} className={`square${idx === 4 ? " active" : ""}`}>
-            {idx === 4 ? "B" : null}
+          <div key={idx} className={`square${idx === index ? " active" : ""}`}>
+            {idx === index ? "B" : null}
           </div>
         ))}
       </div>
       <div className="info">
-        <h3 id="message"></h3>
+        <h3 id="message">{message}</h3>
       </div>
       <div id="keypad">
-        <button onClick={onClick} id="left">
+        <button onClick={moveLeft} id="left">
           SOL
         </button>
-        <button onClick={onClick} id="up">
+        <button onClick={moveUp} id="up">
           YUKARI
         </button>
-        <button onClick={onClick} id="right">
+        <button onClick={moveRight} id="right">
           SAĞ
         </button>
-        <button onClick={onClick} id="down">
+        <button onClick={moveDown} id="down">
           AŞAĞI
         </button>
         <button onClick={reset} id="reset">
           reset
         </button>
       </div>
-      <form>
+      <form onSubmit={onSubmit}>
         <input
           onChange={onChange}
           id="email"
